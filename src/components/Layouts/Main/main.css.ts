9e76379e86a8c28fd7512931af42bf509f65fea9
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-05-20 17:12:22 -0400

    add vanilla extract. add layouts

---
+++ src/components/Layouts/Main/main.css.ts
@@ -0,0 +1,6 @@
+import { style } from "@vanilla-extract/css";
+
+export const main = style({
+  display: "grid",
+  gridTemplateRows: "100px 1fr 100px",
+});
---
+++ src/components/Layouts/index.ts
@@ -0,0 +1,7 @@
+// Barrel file to export multiple Astro components
+
+import Main from "./Main/Main.astro";
+
+export default {
+  Main,
+};
